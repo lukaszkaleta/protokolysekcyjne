0678838e8db45b9e689701a30ea8dee3c61f8f1b
Łukasz Kaleta <lukaszkaleta@gmail.com>
2015-12-05 21:04:07 +0100

    java 7

--- src/main/java/com/ra/dissection/protocol/service/impl/DissectionProtocolServiceImpl.java
+++ src/main/java/com/ra/dissection/protocol/service/impl/DissectionProtocolServiceImpl.java
@@ -25,8 +25,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * @author lukaszkaleta
