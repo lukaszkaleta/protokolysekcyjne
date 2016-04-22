744e83fffd102052ddcf2ae1fbd3ae06c3b8e45c
Łukasz Kaleta <lukaszkaleta@gmail.com>
2016-04-22 21:04:24 +0200

    report name without polish characters

---
+++ src/main/java/com/ra/dissection/protocol/util/AsciiString.java
@@ -0,0 +1,9 @@
+package com.ra.dissection.protocol.util;
+
+/**
+ * Created by lka on 22.04.16.
+ */
+public class AsciiString {
+    public AsciiString(String value) {
+    }
+}
---
+++ src/test/java/com/ra/dissection/protocol/util/AsciiStringTest.java
@@ -0,0 +1,10 @@
+package com.ra.dissection.protocol.util;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lka on 22.04.16.
+ */
+public class AsciiStringTest {
+
+}
